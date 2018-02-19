a6b748830c233628dd5e15e7c616a6bf42788fca
Peter Slavich <peteslavich@gmail.com>
2018-02-19 11:06:14 -0800

    generated sub measurement class

---
+++ BVS/DataModels/SubMeasurement+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  SubMeasurement+CoreDataClass.swift
+//  BVS
+//
+//  Created by Peter Slavich on 2/19/18.
+//  Copyright © 2018 Peter Slavich. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(SubMeasurement)
+public class SubMeasurement: NSManagedObject {
+
+}
