6fe24bf10b22da7039a255d03b002730fbd940da
Peter Slavich <peteslavich@gmail.com>
2018-02-19 11:07:29 -0800

    updated measurement generated class

--- BVS/DataModels/Measurement+CoreDataProperties.swift
+++ BVS/DataModels/Measurement+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Measurement+CoreDataProperties.swift
 //  BVS
 //
-//  Created by Peter Slavich on 2/18/18.
+//  Created by Peter Slavich on 2/19/18.
 //  Copyright © 2018 Peter Slavich. All rights reserved.
 //
 //
@@ -22,5 +22,23 @@ extension Measurement {
     @NSManaged public var uuid: UUID?
     @NSManaged public var patientRating: Int16
     @NSManaged public var patientFeedback: String?
+    @NSManaged public var subMeasurements: NSSet?
+
+}
+
+// MARK: Generated accessors for subMeasurements
+extension Measurement {
+
+    @objc(addSubMeasurementsObject:)
+    @NSManaged public func addToSubMeasurements(_ value: SubMeasurement)
+
+    @objc(removeSubMeasurementsObject:)
+    @NSManaged public func removeFromSubMeasurements(_ value: SubMeasurement)
+
+    @objc(addSubMeasurements:)
+    @NSManaged public func addToSubMeasurements(_ values: NSSet)
+
+    @objc(removeSubMeasurements:)
+    @NSManaged public func removeFromSubMeasurements(_ values: NSSet)
 
 }
