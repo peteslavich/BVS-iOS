474c663d67f6ebbe40d087ffd4cd19d3dae9a743
Peter Slavich <peteslavich@gmail.com>
2018-04-14 10:54:53 -0700

    made measurement encodable

--- BVS/DataModels/Measurement+CoreDataClass.swift
+++ BVS/DataModels/Measurement+CoreDataClass.swift
@@ -11,6 +11,22 @@ import Foundation
 import CoreData
 
 @objc(Measurement)
-public class Measurement: NSManagedObject {
+public class Measurement: NSManagedObject, Encodable {
 
+    enum CodingKeys: String, CodingKey {
+        case measurementOn
+        case volume
+        case uuid
+        case patientRating
+        case patientFeedback
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(measurementOn, forKey: .measurementOn)
+        try container.encode(volume, forKey: .volume)
+        try container.encode(uuid, forKey: .uuid)
+        try container.encode(patientRating, forKey: .patientRating)
+        try container.encode(patientFeedback, forKey: .patientFeedback)
+    }
 }
